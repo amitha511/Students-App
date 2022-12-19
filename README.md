0094a20aa55d9617bf0af54641442743973a6547
amitha511 <amitha51111@gmail.com>
2022-12-19 16:12:50 +0200

    README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Students-App
+
+## Android app
+
+The app contains a list of students with their details and photo.
+Each student has a check box indicating the status of the student.<br>
+pressing the check button will change the check
+status of that student.
+
+# Features:
+
+- Add new student - by the add button in the student list,the user will add a new student to the database.
+- Student details - presents the details of a selected student from the list.
+- Edit student - update a student in the database.
+
+- Delete student - delete a student from the database.
+
+<p align = "center">
+    <img src = https://github.com/amitha511/ticTacToe_android_app/blob/main/studentsapp.gif width="300" height="400" />
--- app/src/main/java/com/example/myapplication/model/Model.java
+++ app/src/main/java/com/example/myapplication/model/Model.java
@@ -9,10 +9,10 @@ public class Model {
 
     private Model(){
         addStudent(new Student("Amit" , "209025689" , "0523338455","Hod Hasharon" , false));
-        addStudent(new Student("Maya" , "384129698" , "0524485566","Telmond" , true));
+        addStudent(new Student("Maya" , "384129698" , "0524485566","Telmond" , false));
         addStudent(new Student("Keren" , "129157285" , "052121111","Tel aviv" , false));
-        addStudent(new Student("Mor" , "123174695" , "0524148225","Hertzeliya" , true));
-        addStudent(new Student("Shoval" , "892356487" , "0523658894","Reshon Lezion" , true));
+        addStudent(new Student("Mor" , "123174695" , "0524148225","Hertzeliya" , false));
+        addStudent(new Student("Shoval" , "892356487" , "0523658894","Reshon Lezion" , false));
         addStudent(new Student("Shay" , "456978153" , "0525558742","Raanana" , false));
         addStudent(new Student("Ben" , "555888777" , "0521597894","TelMond" , false));
         addStudent(new Student("Yuval" , "224588789" , "0522224567","Heifa" , false));
